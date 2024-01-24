da8b4c638bb65526384f38ea2cc6e70043b4d025
Hardik <hardikrajpal@cse.iitb.ac.in>
2024-01-24 10:06:36 +0530

    assignment basics

---
+++ 200050048-pa1/PA1.java
@@ -0,0 +1,5 @@
+public class PA1{
+    public static void main(String args[]){
+        System.out.println("Hello, World!");
+    }
+}
---
+++ 200050048-pa1/testcases/tc1.java
@@ -0,0 +1,13 @@
+public class tc1{
+    public static void main(String args[]){
+        int x,y,z;
+        y = 0;
+        z = 1;
+        x = y+1;
+        z = x+2;
+        if(x<3){
+            z = z+1;
+        }
+        System.out.println(""+x+","+y+","+z);
+    }
+}
